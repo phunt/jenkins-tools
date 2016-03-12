252ae8c56a8fe4cedb811f0b8ce8e23cf2248a91
Patrick Hunt <phunt@cloudera.com>
2016-03-11 16:47:00 -0800

    pprint a specific job info dict

---
+++ list_job_info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from jenkins import Jenkins
+
+from optparse import OptionParser
+import getpass
+
+import re
+
+import pprint
+
+def list_job_info(server, jobName):
+    jobinfo = server.get_job_info(jobName)
+    pprint.pprint(jobinfo)
+    
+
+if __name__ == '__main__':
+    usage = "usage: %prog [options] username jenkinsURL jobName"
+    parser = OptionParser(usage=usage)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 3:
+        parser.error("need username, jenkins URL, and job name")
+    username = args[0]
+    jenkinsURL = args[1]
+    jobName = args[2]
+    
+    password = getpass.getpass(prompt='password? ')
+
+    server = Jenkins(jenkinsURL, username, password)
+    
+    list_job_info(server, jobName)
